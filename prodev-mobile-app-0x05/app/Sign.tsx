e0ea680478fd7f54eff2aa20b6bbb21741745873
Fa23-max <musilijose618@gmail.com>
2025-09-06 20:01:47 +0300

    Mobile app

---
+++ prodev-mobile-app-0x05/app/Sign.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Sign() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Sign Screen - To be updated with proper content</Text>
+    </View>
+  );
+}
